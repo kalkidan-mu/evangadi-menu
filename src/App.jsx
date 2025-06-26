49949239ae5997951a15733232ec10a5dd49bd2d
kalkidan-mu <kalkidan25mulat@gmail.com>
2025-06-26 22:05:47 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+import Header from "./components/Header.jsx";
+import AllFoods from "./components/AllFoods.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AllFoods />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+         <div className="all-container">
+      {/* <!-- header start  --> */}
+      <header className="title">
+        <h1>Evangadi Menu</h1>
+        <div></div>
+      </header>
+      </div>
+      </div>
+    )
+  }
+}
